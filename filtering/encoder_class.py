8d68e57cf28ee3eb7813420f90724fc17491ebc2
jkmin3 <ijoshuamin@gmail.com>
2022-11-01 02:17:41 -0500

    problems with query_paragraph

--- filtering/encoder_class.py
+++ filtering/encoder_class.py
@@ -1,9 +1,10 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch 
+from sentence_transformers import CrossEncoder
 
 class filter(object):
 
-    def scorer(question, answers):
+    def scorer(question:str, answers:list):
         
         model = AutoModelForSequenceClassification.from_pretrained('cross-encoder/ms-marco-MiniLM-L-12-v2')
         tokenizer = AutoTokenizer.from_pretrained('cross-encoder/ms-marco-MiniLM-L-12-v2')
@@ -14,3 +15,13 @@ class filter(object):
         with torch.no_grad():
             scores = model(**features).logits
         return scores
+
+
+    # def query_paragraph(query:str, paragraph:list):
+    #     model = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-12-v2', max_length=512)
+    #     tuples = []
+    #     for p in paragraph:
+    #         tup = (query, p)
+    #         tuples.append(tup)
+    #     scores = model.predict(tuples)
+    #     return scores
